db48935d83907d834d3a8e78b2d5cfde38ee69ef
vit <9455469@mail.ru>
2024-05-22 21:28:12 +0300

    71.3

--- src/svgLineSegments.js
+++ src/svgLineSegments.js
@@ -11,12 +11,12 @@ export class IsometricSvgLineSegments {
     this.groupObjs = isometricSvgElem.getSvgGroup({ container: containerSvg, tag: 'objs' });
   }
 
-  createSegment({ pos1, pos2 }) {
+  createSegment({ pos1, pos2, linkLine, linksObj }) {
     const color = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
 
     const svg = isometricSvgElem.createSvgLine({ x1: pos1.x, y1: pos1.y, x2: pos2.x, y2: pos2.y, stroke: color });
 
-    svg['userData'] = { lineI: true, tag: 'segmentLine', link: null };
+    svg['userData'] = { lineI: true, tag: 'segmentLine', linkLine, linksObj };
 
     this.groupLines.append(svg);
 
@@ -49,21 +49,29 @@ export class IsometricSvgLineSegments {
       if (isometricSvgListObjs.isObjBySvg(svgPoint)) {
         let pos1 = new THREE.Vector2();
         let pos2 = new THREE.Vector2();
+        const linksObj = [];
 
         if (ind === 0) {
           pos1 = isometricSvgElem.getPosLine2(line)[0];
           pos2 = isometricSvgElem.getPosCircle(svgPoint);
+
+          const point = line['userData'].pd1 ? line['userData'].pd1 : line['userData'].p1;
+          linksObj.push(point, svgPoint);
         } else if (ind === arr.length - 1) {
           pos1 = isometricSvgElem.getPosCircle(svgPoint);
           pos2 = isometricSvgElem.getPosLine2(line)[1];
+
+          const point = line['userData'].pd2 ? line['userData'].pd2 : line['userData'].p2;
+          linksObj.push(svgPoint, point);
         } else {
           pos1 = isometricSvgElem.getPosCircle(arr[ind - 1]);
           pos2 = isometricSvgElem.getPosCircle(svgPoint);
+          linksObj.push(arr[ind - 1], svgPoint);
         }
 
-        const line1 = this.createSegment({ pos1, pos2 });
+        const segment = this.createSegment({ pos1, pos2, linkLine: line, linksObj });
 
-        line['userData'].segments.push(line1);
+        line['userData'].segments.push(segment);
       }
 
       if (line['userData'].segments.length > 0) line.setAttribute('display', 'none');
@@ -72,39 +80,16 @@ export class IsometricSvgLineSegments {
 
   // при изменении длины линии, обновляем длину сегментов
   upLineSegments({ line }) {
-    line['userData'].links.sort((a, b) => {
-      return a['userData'].link.dist - b['userData'].link.dist;
-    });
+    line['userData'].segments.forEach((segment) => {
+      const linksObj = segment['userData'].linksObj;
 
-    line['userData'].links.forEach((svgPoint, ind, arr) => {
-      const segment = line['userData'].segments[ind];
-      if (segment) {
-        let pos1 = new THREE.Vector2();
-        let pos2 = new THREE.Vector2();
-        if (ind === 0) {
-          pos1 = isometricSvgElem.getPosLine2(line)[0];
-          pos2 = isometricSvgElem.getPosCircle(svgPoint);
-        } else {
-          pos1 = isometricSvgElem.getPosCircle(arr[ind - 1]);
-          pos2 = isometricSvgElem.getPosCircle(svgPoint);
-        }
+      if (linksObj.length === 2) {
+        const pos1 = isometricSvgElem.getPosCircle(linksObj[0]);
+        const pos2 = isometricSvgElem.getPosCircle(linksObj[1]);
 
         isometricSvgElem.setPosLine2({ svg: segment, x1: pos1.x, y1: pos1.y, x2: pos2.x, y2: pos2.y });
       }
     });
-
-    if (line['userData'].links.length > 0) {
-      const ind = line['userData'].links.length - 1;
-      const svgPoint = line['userData'].links[ind];
-
-      const segment = line['userData'].segments[ind + 1];
-      if (segment) {
-        const pos1 = isometricSvgElem.getPosCircle(svgPoint);
-        const pos2 = isometricSvgElem.getPosLine2(line)[1];
-
-        isometricSvgElem.setPosLine2({ svg: segment, x1: pos1.x, y1: pos1.y, x2: pos2.x, y2: pos2.y });
-      }
-    }
   }
 
   deleteSegment({ segment }) {
